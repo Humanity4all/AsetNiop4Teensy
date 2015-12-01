e8f4b93bf6acb67739e561dfce7ebc8628639736
nihlaeth <info@nihlaeth.nl>
2015-12-01 13:27:15 +0100

    Fixed up docstrings, added OneSwitchUsed state and fixed some typos

--- prototypes/switchboard.py
+++ prototypes/switchboard.py
@@ -1,9 +1,9 @@
-"""SwitchBoard state machine"""
+"""SwitchBoard state machine."""
 
 
 class SwitchBoardState(object):
 
-    """SwitchBoard State prototype"""
+    """SwitchBoard State prototype."""
 
     def press(self):
         """Process switch press."""
@@ -19,9 +19,11 @@ class Idle(SwitchBoardState):
     """No switches are pressed."""
 
     def press(self):
+        """Switch state to OneSwitch + issue KeyDown(nochord)."""
         pass
 
     def release(self):
+        """Impossible - no switches are pressed."""
         pass
 
 
@@ -30,9 +32,24 @@ class OneSwitch(SwitchBoardState):
     """One switch is pressed."""
 
     def press(self):
+        """Switch state to TwoSwitch + issue KeyDown(chord) event."""
         pass
 
     def release(self):
+        """Switch state to Idle + issue KeyDown(chord) & KeyUp(chord) event."""
+        pass
+
+
+class OneSwitchUsed(SwitchBoardState):
+
+    """One switch is pressed, but a key event has been issued already."""
+
+    def press(self):
+        """Switch state to TwoSwitch + KeyDown(nochord)."""
+        pass
+
+    def release(self):
+        """Switch state to Idle + KeyUp(nochord)."""
         pass
 
 
@@ -41,9 +58,11 @@ class TwoSwitch(SwitchBoardState):
     """Two switches are pressed."""
 
     def press(self):
+        """Switch state to Invalid + issue EmptyKey event."""
         pass
 
     def release(self):
+        """Switch state to OneSwitchUsed + issue KeyUp(chord) event."""
         pass
 
 
@@ -58,17 +77,20 @@ class Invalid(SwitchBoardState):
     """
 
     def press(self):
+        """Stay in invalid."""
         pass
 
     def release(self):
+        """Either return to Idle or stay in Invalid."""
         pass
 
 
 class SwitchBoard(object):
 
-    """SwitchBoard state machine"""
+    """SwitchBoard state machine."""
 
     def __init__(self):
+        """Set state to Idle."""
         self.state = Idle()
 
     def press(self):
