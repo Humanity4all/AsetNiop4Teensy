0f55249dea2b2d22c26fc34a80a5502dd5dc2a8c
nihlaeth <info@nihlaeth.nl>
2015-12-09 15:01:45 +0100

    Did some renaming in pininterface

--- src/pininterface/pininterface.h
+++ src/pininterface/pininterface.h
@@ -5,7 +5,7 @@ Copyright 2015 Stichting Humanity4all
 #ifndef SRC_PININTERFACE_PININTERFACE_H_
 #define SRC_PININTERFACE_PININTERFACE_H_
 #include <Bounce2.h>
-#include "../switchboardstatemachine/machine.h"
+#include "../switchboard/machine.h"
 #define N_KEYS 4
 
 namespace pin_interface {
@@ -16,8 +16,8 @@ class PinInterface {
     void update();
     ~PinInterface();
  private:
-    Bounce * mSwitches[N_KEYS];
-    switch_board_state_machine::Machine mSwitchBoard;
+    Bounce * debouncedSwitches[N_KEYS];
+    switch_board::Machine switchBoard;
 };
 
 } // namespace pin_interface
