1e86d255a4ec499d2eda3f5ef6cc99031c9b4320
nihlaeth <info@nihlaeth.nl>
2015-12-12 13:58:06 +0100

    WIP: implementing SwitchEvent

---
+++ src/switchevent/switchevent.cpp
@@ -0,0 +1,65 @@
+/*
+Copyright Stichting Humanity4all 2015
+*/
+#include "./switchevent.h"
+
+namespace switch_event_n {
+
+SwitchEvent::SwitchEvent(
+        switch_state_t old_switch_state[N_SWITCHES],
+        switch_state_t new_switch_state[N_SWITCHES]) {
+    oldSwitchState = old_switch_state;
+    newSwitchState = new_switch_state;
+    this->update_effective_switch_state();
+}
+
+void SwitchEvent::new_event(
+        switch_state_t new_switch_state[N_SWITCHES],
+        SwitchEvent* return_event) {
+    &return_event = new SwitchEvent(newSwitchState, new_switch_state);
+}
+
+int SwitchEvent::count_active() {
+    int acc = 0;
+    for (int i = 0; i < N_SWITCHES; i++) {
+        if (effectiveSwitchState[i] == 1) {
+            acc += 1;
+        }
+    }
+    return acc;
+}
+
+int SwitchEvent::get_active_switch(int n) {
+    int nth = 0;
+    for (int i = 0; i < N_SWITCHES; i++) {
+        if (effectiveSwitchState[i] == 1) {
+            nth += 1;
+            if (n == nth) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int SwitchEvent::state_diff() {
+    for (int i = 0; i < N_SWITCHES; i++) {
+        if ((oldSwitchState[i] ^ newSwitchState[i]) == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+SwitchEvent::~SwitchEvent() {
+}
+
+void SwitchEvent::update_effective_switch_state() {
+    for (int i = 0; i < N_SWITCHES; i++) {
+        if (oldSwitchState[i] || newSwitchState[i]) {
+            effectiveSwitchState[i] = switch_state_t::PRESSED;
+        }
+    }
+}
+
+} // namespace switch_event_n
--- src/switchevent/switchevent.h
+++ src/switchevent/switchevent.h
@@ -11,13 +11,17 @@ Copyright 2015 Stichting Humanity4all
 namespace switch_event_n {
 
 typedef enum switch_state_t {
-    PRESSED, RELEASED,
+    PRESSED = 1 , RELEASED = 0,
 } key_state_t;
 
 class SwitchEvent {
  public:
-    SwitchEvent();
-    void new_event(switch_state_t new_switch_state, SwitchEvent* return_event);
+    SwitchEvent(
+        switch_state_t new_switch_state[N_SWITCHES],
+        switch_state_t old_switch_state[N_SWITCHES]);
+    void new_event(
+        switch_state_t new_switch_state[N_SWITCHES],
+        SwitchEvent* return_event);
     int count_active();
     int get_active_switch(int n=1);
     int state_diff();
@@ -25,9 +29,9 @@ class SwitchEvent {
     edge_t edge;
  private:
     void update_effective_switch_state();
-    const switch_state_t oldSwitchState[N_SWITCHES];
-    const switch_state_t newSwitchState[N_SWITCHES];
-    const switch_state_t effectiveSwitchState[N_SWITCHES];
+    switch_state_t oldSwitchState[N_SWITCHES];
+    switch_state_t newSwitchState[N_SWITCHES];
+    switch_state_t effectiveSwitchState[N_SWITCHES];
 };
 
 } // namespace switch_event_n
