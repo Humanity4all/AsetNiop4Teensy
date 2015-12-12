c4889137f65bf7bb23edb6266088829dcc24eef4
nihlaeth <info@nihlaeth.nl>
2015-12-12 12:48:48 +0100

    Implement TwoSwitch

--- src/switchboard/machinestates.cpp
+++ src/switchboard/machinestates.cpp
@@ -71,6 +71,32 @@ void OneSwitch::release(
 OneSwitch::~OneSwitch() {
 }
 
+void TwoSwitch::press(
+        Machine& machine,
+        switch_event_n::SwitchEvent* switch_event,
+        protokey_event_t* return_event) {
+    /*
+     * TODO: this method should return 2 events, one down and one reset
+     */
+    return_event->event = event_t::DOWN;
+    return_event->switch_event = switch_event;
+    return_event->is_chord = false;
+    machine.change_state(new ManySwitch());
+}
+
+void TwoSwitch::release(
+        Machine& machine,
+        switch_event_n::SwitchEvent* switch_event,
+        protokey_event_t* return_event) {
+    return_event->event = event_t::UP;
+    return_event->switch_event = switch_event;
+    return_event->is_chord = true;
+    machine.change_state(new OneSwitchUsed());
+}
+
+TwoSwitch::~TwoSwitch() {
+}
+
 
 
 
