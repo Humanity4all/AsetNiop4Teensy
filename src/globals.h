96273e9b3c2a7954d832b2b586cd7b5bed686949
nihlaeth <info@nihlaeth.nl>
2015-12-16 19:00:42 +0100

    Undo unimplemented functions, this is not the place (read: lots of errors

--- src/globals.h
+++ src/globals.h
@@ -18,15 +18,4 @@ Copyright 2015 Stichting Humanity4all
   #define LED_PIN 13
 #endif
 
-
-/*
- * Fix unimplemented errors for calling template functions with unrestricted
- * arguments (read: we got in trouble with the event queues)
- */
-extern "C" {
-    int _getpid() { return -1; }
-    int _kill(int pid, int sig) { return -1; }
-    int _write() { return -1; }
-}
-
 #endif // SRC_GLOBALS_H_
