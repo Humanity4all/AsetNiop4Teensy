7140d8de75cd198012303cd77c107248b5c264e0
nihlaeth <info@nihlaeth.nl>
2015-12-16 14:25:54 +0100

    Adjust pin settings for testing

--- src/globals.h
+++ src/globals.h
@@ -5,14 +5,14 @@ Copyright 2015 Stichting Humanity4all
 #ifndef SRC_GLOBALS_H_
 #define SRC_GLOBALS_H_
 
-#define N_SWITCHES 4
+#define N_SWITCHES 3
 /*
  * This tells us which pins correspond to switches.
  * It's assumed that left-most value corresponds to the
  * left pinky, the thumbs are in the middle, and the right
  * pinky is the last value.
  */
-#define SWITCH_PINS 1, 3, 5, 7
+#define SWITCH_PINS 0, 1, 2
 
 #ifndef LED_PIN
   #define LED_PIN 13
