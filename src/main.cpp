9fca15788a5d5a4e656b0460fdbbf932208278ed
nihlaeth <info@nihlaeth.nl>
2015-12-16 14:16:07 +0100

    Call pininterface for testing

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ Copyright 2015 Stichting Humanity4all
 #include "./switchboard/machine.h"
 #include "./switchboard/typedefs.h"
 #include "./switchevent/switchevent.h"
+#include "./pininterface/pininterface.h"
 
 #define DEBUG
 
@@ -28,27 +29,25 @@ Bounce debugkey;
 std::queue<switch_event_n::SwitchEvent> switch_event_queue;
 std::queue<switch_board_n::protokey_event_t> protokey_event_queue;
 
+pin_interface_n::PinInterface pin_interface;
 switch_board_n::Machine switch_machine;
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);     // set pin as output
+    // pinMode(LED_PIN, OUTPUT);     // set pin as output
     // our meager debug key - just so we know it's doing something
-    pinMode(0, INPUT_PULLUP);
-    debugkey.attach(0);
-    debugkey.interval(10);
+    // pinMode(0, INPUT_PULLUP);
+    // debugkey.attach(0);
+    // debugkey.interval(10);
 
-    int pinsToUse[N_SWITCHES]={1, 2, 3, 4};
-    for (int i=0; i < N_SWITCHES; i++) {
-        int pin = pinsToUse[i];
-        pinMode(pin, INPUT_PULLUP);
-    }
+    pin_interface.init_pins();
 }
 
 void loop() {
 
     // check if it's responding at all
-    if (debugkey.update()) {
-        if (debugkey.read() == 0) digitalWrite(LED_PIN, HIGH); // pressed
-        if (debugkey.read() == 1) digitalWrite(LED_PIN, LOW);  // released
-    }
+    // if (debugkey.update()) {
+    //    if (debugkey.read() == 0) digitalWrite(LED_PIN, HIGH); // pressed
+    //    if (debugkey.read() == 1) digitalWrite(LED_PIN, LOW);  // released
+    // }
+    pin_interface.update(&switch_event_queue);
 }
