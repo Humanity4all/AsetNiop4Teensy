d9e0ad9354c2edf77e92b7d00bfd5938b45a7061
nihlaeth <info@nihlaeth.nl>
2015-12-16 13:57:42 +0100

    Add pin number list for Bounce initialization

--- src/globals.h
+++ src/globals.h
@@ -6,5 +6,12 @@ Copyright 2015 Stichting Humanity4all
 #define SRC_GLOBALS_H_
 
 #define N_SWITCHES 4
+/*
+ * This tells us which pins correspond to switches.
+ * It's assumed that left-most value corresponds to the
+ * left pinky, the thumbs are in the middle, and the right
+ * pinky is the last value.
+ */
+#define SWITCH_PINS 1, 3, 5, 7
 
 #endif // SRC_GLOBALS_H_
