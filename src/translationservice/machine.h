80d7dfff99063ef6a4836f83c510e647a22479af
nihlaeth <info@nihlaeth.nl>
2015-12-19 12:11:32 +0100

    Correct header guard and fix some style issues

--- src/translationservice/machine.h
+++ src/translationservice/machine.h
@@ -3,7 +3,7 @@ Copyright 2015 Stichting Humanity4all
 */
 
 #ifndef SRC_TRANSLATIONSERVICE_MACHINE_H_
-#define SRC_SWITCHBOARD_MACHINE_H_
+#define SRC_TRANSLATIONSERVICE_MACHINE_H_
 
 #include "../globals.h"
 #include "./typedefs.h"
@@ -15,18 +15,18 @@ namespace translation_service_n {
 class AbstractState;
 
 class Machine {
-    public:
-      Machine();
-      void process_protokey_event(
-            switch_board::protokey_event_t protokey_event);
-      void change_state(AbstractState new_state);
-      void send_key(key_t key, switch_board::event_t event);
-      void set_modifier(key_t key);
-      int[] use_modifiers();
-    private:
-      AbstractState currentState;
+ public:
+    Machine();
+    void process_protokey_event(
+        switch_board::protokey_event_t protokey_event);
+    void change_state(AbstractState new_state);
+    void send_key(key_t key, switch_board::event_t event);
+    void set_modifier(key_t key);
+    int[] use_modifiers();
+ private:
+    AbstractState currentState;
 };
 
 } // namespace translation_service_n
 
-#endif // SRC_TRANSLATION_SERVICE_MACHINE_H_
+#endif // SRC_TRANSLATIONSERVICE_MACHINE_H_
