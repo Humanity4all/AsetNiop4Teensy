bf6a3e611a77093c50f5a05b11b4971404581a15
nihlaeth <info@nihlaeth.nl>
2016-01-03 15:25:08 +0100

    Added layer to addressing (oops...)

--- src/keymap/getkey.cpp
+++ src/keymap/getkey.cpp
@@ -6,9 +6,13 @@ Copyright 2016 Stichting Humanity4all
 
 namespace keymap_n {
 
-translation_service_n::key_t get_key(int switch1, int switch2) {
+translation_service_n::key_t get_key(int layer, int switch1, int switch2) {
     // TODO kick watchdog if switch1 or switch2 >= N_SWITCHES
-    int address = switch1 * N_SWITCHES + switch2 + EEPROM_SETTINGS_OFFSET;
+    int address =
+        layer * N_SWITCHES * N_SWITCHES +
+        switch1 * N_SWITCHES +
+        switch2 +
+        EEPROM_SETTINGS_OFFSET;
     return (translation_service_n::key_t) EEPROM.read(address);
 }
 
