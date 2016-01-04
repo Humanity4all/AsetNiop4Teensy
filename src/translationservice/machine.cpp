cfdc75cf56c95b8a906c996d62b949c6cc522fce
nihlaeth <info@nihlaeth.nl>
2016-01-04 13:21:45 +0100

    Basic implementation of Machine::send_key

--- src/translationservice/machine.cpp
+++ src/translationservice/machine.cpp
@@ -28,6 +28,54 @@ void Machine::send_key(key_t key, switch_board_n::event_t event) {
     Serial.print(" ");
     Serial.println(event);
     #endif
+    char key_code;
+    switch (key) {
+        case key_t::K_A: {
+            key_code = KEY_A;
+            break;
+        }
+        case key_t::K_B: {
+            key_code = KEY_B;
+            break;
+        }
+        case key_t::K_C: {
+            key_code = KEY_C;
+            break;
+        }
+        case key_t::K_D: {
+            key_code = KEY_D;
+            break;
+        }
+        case key_t::K_E: {
+            key_code = KEY_E;
+            break;
+        }
+        case key_t::K_F: {
+            key_code = KEY_F;
+            break;
+        }
+        default: {
+            // TODO kick watchdog
+            #ifdef DEBUG
+            Serial.print("translation_service: unrecognized key: ");
+            Serial.println(key);
+            #endif
+        }
+    }
+
+    if (event == switch_board_n::event_t::UP) {
+        Keyboard.release(key);
+    } else if (event == switch_board_n::event_t::DOWN) {
+        Keyboard.press(key);
+    } else if (event == switch_board_n::event_t::RESET) {
+        Keyboard.releaseAll();
+    } else {
+        // TODO kick watchdog
+        #ifdef DEBUG
+        Serial.print("translation_service: unrecognized event: ");
+        Serial.println(event);
+        #endif
+    }
 }
 
 void Machine::set_modifier(key_t key) {
