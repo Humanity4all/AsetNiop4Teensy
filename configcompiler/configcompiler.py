6c5347fb64c2ff2d0630b6750559c5ae2ea1a5de
Sietse Brouwer <sbbrouwer@gmail.com>
2016-03-04 08:46:02 +0100

    Add comments to configcompiler

--- configcompiler/configcompiler.py
+++ configcompiler/configcompiler.py
@@ -6,14 +6,22 @@ N_SWITCHES = 8
 
 
 def populate_buffer(offset, n_switches, values):
+    # The values will be packed into a little-endian struct of n*n bytes
     s = struct.Struct('<' + 'b ' * n_switches * n_switches)
+    # The actual bytearray consists of some front matter + the values struct
     buf = bytearray(s.size + OFFSET)
+    # Write the values struct into the right end of the bytearray
     s.pack_into(buf, offset, *values)
     return buf
 
+# Create a value for every switch combination. In this case, just
+# the numbers 1..256
 values = range(N_SWITCHES**2)
+
+# Create a buffer of front matter + values
 buf = populate_buffer(OFFSET, N_SWITCHES, values)
 
+# Print the hex of that buffer to output
 print(binascii.hexlify(buf))
 
 # TODO: write the result to a file, and inspect the file with hexedit.
