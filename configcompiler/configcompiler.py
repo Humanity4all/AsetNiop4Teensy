80205e520d1c8676ad70bd82bc7d559d6f8d79ba
Sietse Brouwer <sbbrouwer@gmail.com>
2016-03-19 22:43:34 +0100

    configcompiler fix: use offset param instead of global

--- configcompiler/configcompiler.py
+++ configcompiler/configcompiler.py
@@ -10,9 +10,9 @@ def populate_buffer(values, offset=OFFSET, n_switches=N_SWITCHES):
         n=n_switches, n2=n_switches**2, actual_n=len(values))
     assert len(values) == n_switches**2, error_message
     # The values will be packed into a little-endian struct of n*n bytes
-    s = struct.Struct('<' + 'b ' * n_switches * n_switches)
+    s = struct.Struct('<' + 'b ' * n_switches**2)
     # The actual bytearray consists of some front matter + the values struct
-    buf = bytearray(s.size + OFFSET)
+    buf = bytearray(s.size + offset)
     # Write the values struct into the right end of the bytearray
     s.pack_into(buf, offset, *values)
     return buf
